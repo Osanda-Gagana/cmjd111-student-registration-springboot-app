b4f03cfb727390c31f817d517f9be6b6278ddcd8
OsandaGagana <Osandagds@gmail.com>
2025-07-13 10:23:04 +0530

    path variable updates in the student controller

--- src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/controller/StudentController.java
+++ src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/controller/StudentController.java
@@ -1,12 +1,11 @@
 package lk.ijse.cmjd_111.CourseRegistration2025.controller;
 
+import lk.ijse.cmjd_111.CourseRegistration2025.dto.Role;
 import lk.ijse.cmjd_111.CourseRegistration2025.dto.UserDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/student")
@@ -17,7 +16,24 @@ public class StudentController {
     //}
 
     @PostMapping
-    public ResponseEntity<UserDTO> saveStudent(@RequestBody UserDTO userDTO){
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public ResponseEntity<UserDTO> saveStudent(@RequestBody UserDTO studentData){
+        System.out.println(studentData);
+        return new ResponseEntity<>(studentData,HttpStatus.CREATED);
     }
+
+    @GetMapping("{studentID}")
+    public ResponseEntity<UserDTO> getSelectedStudent(@PathVariable String studentID){
+        // send student details
+        System.out.println("Path Variable is: "+studentID);
+        var student01 = new UserDTO("STU01", "Kamal",
+                "Perera", "kamal@mail.com",
+                "First Street", "Panadura South",
+                "Panadura", "Panadura",
+                "123", Role.STUDENT);
+        if(studentID == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(student01,HttpStatus.OK);
+    }
+
 }
--- src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/dto/Role.java
+++ src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/dto/Role.java
@@ -1,5 +1,5 @@
 package lk.ijse.cmjd_111.CourseRegistration2025.dto;
 
 public enum Role {
-
+    STUDENT,LECTURER,ADMIN
 }
